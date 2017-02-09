9fdb129c3bf124e7706c1dc8eba40bd2adb6a1ea
Maxime Cantin <maxime_cantin@yahoo.com>
2017-02-09 23:28:09 +0000

    Generated dynamic HTML table from Transactions

--- app/drag-drop.js
+++ app/drag-drop.js
@@ -1,4 +1,4 @@
-/* eslint-env browser */
+/* eslint-env browser */';';
 
 var budgetItemsByPattern = null;
 
@@ -27,15 +27,49 @@ dropZone.ondrop = function(e) {
   var files = e.dataTransfer.files;
   
   for(var i = 0; i < files.length; ++i) {
+    
     outputZone.innerHTML = "";
     logger.info("ondrop() : processing file \'" + files[i].name + "' file");
+    
     FileHelper.processAsText(files[i], function(text) {
+      
       var lines = new CSVParser().parse(text);
-      var headers = ['date', 'description', 'amount', 'account', 'person', 'budgetItem'];
-      var output = new TransactionWriter().write(new BudgetItemFilter().process(new TransactionFilter().process(lines)), headers);
-      output = output.replace(/\n/g, "\n<br/>");
+      var transactions = new BudgetItemFilter().process(new TransactionFilter().process(lines));
+      
+      var output = "";
+      
+      transactions.forEach(function(transaction) {
+        output += "<tr class='";
+        output += transaction.budgetItem !== '' ? "" : "danger";
+        output += "'><td>";
+        output += "<input type='checkbox' value=''>"
+        output += "</td><td>";        
+        output += transaction.date;
+        output += "</td><td>";
+        output += transaction.description;
+        output += "</td><td>";
+        output += transaction.amount;
+        output += "</td><td>";
+        output += transaction.account;
+        output += "</td><td>";
+        output += transaction.person;
+        output += "</td><td>";
+        output += transaction.budgetItem;
+        // output += "</td><td>";
+        // output += '<div class="dropdown">';
+        // output += '<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">';
+        // output += 'Actions';
+        // output += '<span class="caret"></span>';
+        // output += '</button>';
+        // output += '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">';
+        // output += '<li><a href="#">Delete</a></li>';
+        // output += '</ul>';
+        // output += '</div>';
+        output += "</td></tr>";
+      });
+      
       outputZone.innerHTML += output;
-      outputZone.innerHTML += '<br/>';
+      
     });
   }
 
